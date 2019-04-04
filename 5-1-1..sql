754303fcc328b36b59ca6bd184be0d0851181e43
Hellop2k <48230446+Unnnaa@users.noreply.github.com>
2019-04-05 00:57:52 +0800

    Update 5-1-1..sql

--- 5-1-1..sql
+++ 5-1-1..sql
@@ -1,22 +1,10 @@
-BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "logins" (
-	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-	"email"	TEXT,
-	"password"	INTEGER
-);
-CREATE TABLE IF NOT EXISTS "majors" (
-	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-	"name"	INTEGER,
-	"credits"	INTEGER,
-	"student_id"	INTEGER
-);
-CREATE TABLE IF NOT EXISTS "students" (
-	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-	"name"	TEXT,
-	"phone"	INTEGER,
-	"login_id"	INTEGER
-);
-INSERT INTO "logins" ("id","email","password") VALUES (1,'john@gmail.com','abc123');
-INSERT INTO "majors" ("id","name","credits","student_id") VALUES (1,'CS',120,1);
-INSERT INTO "students" ("id","name","phone","login_id") VALUES (1,'John Park',99991234,1);
-COMMIT;
+SELECT
+logins.email,
+logins.password,
+students.name,
+students.phone,
+majors.name,
+majors.credits
+from logins
+JOIN students on logins.id = students.login_id
+JOIN majors on students.id = majors.student_id
